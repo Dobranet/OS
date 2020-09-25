b7b8f50eadf8f1213f959638aecb7ccf6b12b43c
Dobranet <kev_0011@mail.ru>
2020-09-25 17:19:22 +0300

    new loh.sh

--- log.sh
+++ log.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 function log () {
-warning="$(cat /var/log/anaconda/X.log | awk '{if ($3 == "(WW)")print $0}')"
-inf="$(cat /var/log/anaconda/X.log | awk '{if ($3 == "(||)")printf $0}')"
+warning="$(cat /var/log/anaconda/X.log | awk '{if (($3 == "(WW)")&&($1 == "["))print $0}')"
+inf="$(cat /var/log/anaconda/X.log | awk '{if (($3 == "(||)")&&($1 == "["))printf $0}')"
 
 echo -e ${warning//"(WW)"/"\e[1;33mWarning:\e[0m"}
 echo -e ${inf//"(||)"/"\e[1;34mInformation:\e[0m"}
